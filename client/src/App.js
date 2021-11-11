64bd8c630cedafa8da20b1cc1d460b07547b0cfe
John <jddhalogamer@gmail.com>
2021-11-11 15:47:55 -0600

    added Button component, and state to App.js.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
+import { useState } from 'react'
 import Header from './components/Header'
+import Button from './components/Button'
 import InputForm from './components/InputForm'
 
 const App = () => {
+
+  const [ showSearchForm, setShowSearchForm ] = useState(false)
+
   return (
     <div className="container">
       <Header />
-      <InputForm />
+      <Button text='Search PokeDex' />
+      {!showSearchForm ? '' : <InputForm />}
     </div>
   );
 }
