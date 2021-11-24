a8472d0c935ae2a9bb6e1cfd0cce9e4fb08813a8
John <jddhalogamer@gmail.com>
2021-11-23 19:00:23 -0600

    refactored code, and app now fetches and displays 20 results..

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import Header from './components/Header'
 import Button from './components/Button'
 import InputForm from './components/InputForm'
+import PokemonThumb from './components/PokemonThumb'
 
 const App = () => {
 
@@ -12,21 +13,22 @@ const App = () => {
 
 
   const getAllPokemon = async () => {
-    const res = await axios.get(loadMore)
+    const res = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=20')
     const data = res.data
+    const results = res.data.results
 
     setLoadMore(data.next)
-    
-    function createPokemonObject (results) {
-      results.forEach(async (pokemon) => {
-        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon${pokemon.name}`)
-        const data = await res.data
 
-        setAllPokemon(currentList => [...currentList, data])
-        allPokemon.push(data)
+    function createPokemonObject(results) {
+      results.forEach(async (pokemon) => {
+        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+        const data = res.data
+
+        setAllPokemon( currentList => [...currentList, data])
+        await allPokemon.sort((a, b) => a.index - b.index)
       })
     }
-    createPokemonObject(data.result)
+    createPokemonObject(data.results)
   }
 
   useEffect(() => {
@@ -42,7 +44,19 @@ const App = () => {
       />
       {!showSearchForm ? '' : <InputForm />}
       <div className="pokemon-container">
-        <div className="all-container"></div>
+        <div className="all-container">
+          {allPokemon.map( (pokemonStats, index) => 
+          <PokemonThumb 
+          key={index}
+          id={pokemonStats.id}
+          image={pokemonStats.sprites.other.dream_world.front_default}
+          name={pokemonStats.name}
+          type={pokemonStats.types[0].type.name}
+          />)
+        }
+
+
+        </div>
         <Button text='Load More...' />
       </div>
     </div>
